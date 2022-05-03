f44affd46bb8d3a16af73a032506b2badbdd66ef
Takanori Suzuki <takanori@takanory.net>
2022-05-03 18:25:12 +0900

    add thread support

--- plugins/misc.py
+++ plugins/misc.py
@@ -6,7 +6,7 @@ from slack_bolt import App, BoltContext, Say
 
 def enable_misc_plugin(app: App) -> None:
     @app.message(compile(r"^\$choice\s+(.*)"))
-    def choice(say: Say, context: BoltContext) -> None:
+    def choice(message: dict, say: Say, context: BoltContext) -> None:
         """choice and return one of the specified keywords"""
 
         choiced = ""
@@ -16,20 +16,19 @@ def enable_misc_plugin(app: App) -> None:
             choiced = random.choice(words[0])
         else:
             choiced = random.choice(words)
-        say(choiced)
+        say(choiced, thread_ts=message.get("thread_ts"))
 
     @app.message(compile(r"^\$shuffle\s+(.*)"))
-    def shuffle(say: Say, context: BoltContext) -> None:
+    def shuffle(message: dict, say: Say, context: BoltContext) -> None:
         """Shuffle and return specified keywords"""
         words = context["matches"][0].split()
         if len(words) == 1:
             words = list(words[0])
 
         random.shuffle(words)
-        say(" ".join(words))
+        say(" ".join(words), thread_ts=message.get("thread_ts"))
 
     @app.message(compile(r"^\$ping$"))
     def ping(message: dict, say: Say) -> None:
         """return pong in response to ping"""
-        thread_ts = message.get("thread_ts")
-        say("pong", thread_ts=thread_ts)
+        say("pong", thread_ts=message.get("thread_ts"))
